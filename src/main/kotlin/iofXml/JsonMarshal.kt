17a378cdb88cb0f0ae1ed6cbc5eb86b8b94f42e4
mikaello <2505178+mikaello@users.noreply.github.com>
2026-04-04 18:18:29 +0200

    fix: replace deprecated ObjectMapper.setSerializationInclusion with JsonMapper builder API
    ObjectMapper.setSerializationInclusion was deprecated in Jackson 2.13.
    Using JsonMapper.builder().serializationInclusion() is the recommended
    replacement and eliminates the compiler deprecation warning.

--- src/main/kotlin/iofXml/JsonMarshal.kt
+++ src/main/kotlin/iofXml/JsonMarshal.kt
@@ -3,6 +3,7 @@ package iofXml
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
 import java.util.Locale
 import java.util.TimeZone
 import kotlin.collections.HashMap
@@ -103,11 +104,12 @@ internal fun iofJsonToXml(json: String, iofVersion: String = "v3"): String {
  * @sample iofXml.JsonMarshalKtTest.marshalIofObjectToJson
  */
 fun marshalIofObjectToJson(obj: Any, prettyPrint: Boolean = true): String {
-    val mapper = ObjectMapper()
-    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL) // Else all fields not set will be 'null'
+    val builder = JsonMapper.builder()
+        .serializationInclusion(JsonInclude.Include.NON_NULL)
     if (prettyPrint) {
-        mapper.enable(SerializationFeature.INDENT_OUTPUT)
+        builder.enable(SerializationFeature.INDENT_OUTPUT)
     }
+    val mapper = builder.build()
 
     //mapper.enable(SerializationFeature.WRAP_ROOT_VALUE) // Problem: root will be UpperCamelCase (need lowerCamelCase)
     val className = nameFromJavaClass(obj.javaClass)
