4bcdec68ec73707b363878d7dc9a446388c0b4c0
mikaello <mikaello@users.noreply.github.com>
2026-09-25 13:01:10 +0000

    deploy: fcd13ba4f31be58f4369ceb3b3a3e6f0bc574f32

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"iofXml.v2"},{"l":"iofXml.v3"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"iofXml.v2"},{"l":"iofXml.v3"}];updateSearchResults();
